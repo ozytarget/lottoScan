e11bf911e11f6f2acd51cf46621b5722dd587873
Lottery Scanner <urbin@lotteryscanner.com>
2025-12-11 20:55:09 -0500

    Update Railway configuration with environment variables and headless settings

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-streamlit run lottoScan.py --server.port=$PORT --server.address=0.0.0.0
+streamlit run lottoScan.py \
+  --server.port=$PORT \
+  --server.address=0.0.0.0 \
+  --server.headless=true \
+  --server.enableCORS=false \
+  --server.enableXsrfProtection=false \
+  --logger.level=info
+
